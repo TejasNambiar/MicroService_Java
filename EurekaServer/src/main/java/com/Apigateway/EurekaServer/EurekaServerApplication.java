9e64a307d661d255107f5f4406941b4dd48ae1ca
TejasASDF <tejasnambiar32@gmail.com>
2021-11-04 10:57:49 +0530

    Eureka_Server

---
+++ EurekaServer/src/main/java/com/Apigateway/EurekaServer/EurekaServerApplication.java
@@ -0,0 +1,24 @@
+package com.Apigateway.EurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurerAdapter() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/service").allowedOrigins("http://192.168.0.101:8083");
+//			}
+//		};
+//	}
+}
